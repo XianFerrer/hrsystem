0fa675f2fb6d81b019abbdc233fc9ebf35b9408b
christian ferrer <142543067+XianFerrer@users.noreply.github.com>
2024-10-26 22:11:47 +0800

    Delete hrmsystem directory

--- hrmsystem/generate_hash.php
+++
@@ -1,7 +0,0 @@
-<?php
-$password = 'admin123'; // The plain text password
-$hashed_password = password_hash($password, PASSWORD_DEFAULT); // Generate the hash
-
-echo "Plain Password: " . $password . "<br>";
-echo "Hashed Password: " . $hashed_password;
-?>
--- hrmsystem/includes/header.php
+++
@@ -1,7 +0,0 @@
-<!-- header.php -->
-<header>
-    
-    <div class="header">
-        <h1>HR Management System</h1>
-    </div>
-</header>
